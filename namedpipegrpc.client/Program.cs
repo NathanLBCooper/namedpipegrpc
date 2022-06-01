8f2c3f7b960d5bcbf0669cdbe05eb42beb1cf199
nathanlbcooper <nathan.lb.cooper@outlook.com>
2022-06-01 23:57:57 +0200

    Create simple example

---
+++ namedpipegrpc.client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using GrpcDotNetNamedPipes;
+using namedpipegrpc.defs;
+
+namespace namedpipegrpc.client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = new NamedPipeChannel(".", "pipe_for_maths");
+            var client = new Maths.MathsClient(channel);
+
+            while (true)
+            {
+                var n = int.Parse(Console.ReadLine());
+                var m = int.Parse(Console.ReadLine());
+
+                var response = await client.AddAsync(new AddRequest { N = n, M = m });
+
+                Console.WriteLine($"{n} plus {m} is {response.N}");
+            }
+        }
+    }
+}
---
+++ namedpipegrpc.server/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcDotNetNamedPipes;
+using namedpipegrpc.defs;
+
+namespace namedpipegrpc.server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new NamedPipeServer("pipe_for_maths");
+            Maths.BindService(server.ServiceBinder, new MathsService());
+            server.Start();
+        }
+    }
+
+    class MathsService : Maths.MathsBase
+    {
+        public override async Task<AddReply> Add(AddRequest request, ServerCallContext context)
+        {
+            await Task.CompletedTask;
+            return new AddReply
+            {
+                N = request.N + request.M
+            };
+        }
+    }
+}
